29398b0470d3f2abebcf8d26065e2893e30accf8
tarunTCXR <tarunsohal587@gmail.com>
2025-08-29 23:47:03 -0700

    modified base models a bit in ORM

--- twitter_sentiment_webapp/sentiment/models.py
+++ twitter_sentiment_webapp/sentiment/models.py
@@ -6,3 +6,16 @@ class SentimentAnalysis(models.Model):
 
     def __str__(self):
         return f'Tweet ID: {self.tweet_id}, Sentiment Score: {self.sentiment_score}'
+    
+class Person(models.Model):
+    name = models.CharField(max_length=255)
+    user = models.CharField(max_length=255, unique = True)
+    # We've already defined the related name as a foreign key on Post
+
+class Post(models.Model):
+    post_id = models.CharField(max_length=255, unique=True)
+    author = models.CharField(max_length=255)
+    person = models.ForeignKey(Person, on_delete=models.CASCADE, related_name='posts')
+    text = models.TextField()
+    created_at = models.DateTimeField()
+    sentiment = models.ForeignKey()
